e30bc91920f0383e026abb3f0acfcf8e49da80de
Matthew Fainman <mattf96@gmail.com>
2023-08-14 11:54:05 +0200

    update sst versions

--- packages/my-remix-app/server.js
+++ packages/my-remix-app/server.js
@@ -1,24 +1,21 @@
 import * as fs from "node:fs";
 
-import type {
-  GetLoadContextFunction,
-  RequestHandler,
-} from "@remix-run/express";
 import { createRequestHandler } from "@remix-run/express";
-import type { ServerBuild } from "@remix-run/node";
 import { broadcastDevReady, installGlobals } from "@remix-run/node";
 import chokidar from "chokidar";
 import compression from "compression";
 import express from "express";
 import morgan from "morgan";
+import sourceMapSupport from "source-map-support";
 
+sourceMapSupport.install();
 installGlobals();
 
 const BUILD_PATH = "./build/index.js";
 /**
  * @type { import('@remix-run/node').ServerBuild | Promise<import('@remix-run/node').ServerBuild> }
  */
-const build = await import(BUILD_PATH);
+let build = await import(BUILD_PATH);
 
 const app = express();
 
@@ -37,23 +34,15 @@ app.use(
 // more aggressive with this caching.
 app.use(express.static("public", { maxAge: "1h" }));
 
-morgan.token("url", (req, res) => decodeURIComponent(req.url ?? ""));
 app.use(morgan("tiny"));
-
-const getLoadContextFn: GetLoadContextFunction = async (req, res) => {
-  // can do dependency injection: https://sergiodxa.com/articles/dependency-injection-in-remix-loaders-and-actions
-  // or add a nonce: https://github.com/epicweb-dev/epic-stack/blob/main/server/index.ts#:~:text=123
-  return {};
-};
 
 app.all(
   "*",
   process.env.NODE_ENV === "development"
-    ? createDevRequestHandler(build)
+    ? createDevRequestHandler()
     : createRequestHandler({
         build,
         mode: process.env.NODE_ENV,
-        getLoadContext: getLoadContextFn,
       })
 );
 
@@ -66,43 +55,26 @@ app.listen(port, async () => {
   }
 });
 
-/**
- * @param {ServerBuild} initialBuild
- */
-function createDevRequestHandler(initialBuild: ServerBuild): RequestHandler {
-  let build = initialBuild;
-  async function handleServerUpdate() {
-    // 1. re-import the server build
-    build = await reimportServer();
-    // 2. tell dev server that this app server is now up-to-date and ready
-    broadcastDevReady(build);
-  }
+function createDevRequestHandler() {
+  const watcher = chokidar.watch(BUILD_PATH, { ignoreInitial: true });
 
-  chokidar
-    .watch(BUILD_PATH, { ignoreInitial: true })
-    .on("add", handleServerUpdate)
-    .on("change", handleServerUpdate);
+  watcher.on("all", async () => {
+    // 1. purge require cache && load updated server build
+    const stat = fs.statSync(BUILD_PATH);
+    build = import(BUILD_PATH + "?t=" + stat.mtimeMs);
+    // 2. tell dev server that this app server is now ready
+    broadcastDevReady(await build);
+  });
 
   return async (req, res, next) => {
     try {
+      //
       return createRequestHandler({
-        build,
+        build: await build,
         mode: "development",
-        getLoadContext: getLoadContextFn,
       })(req, res, next);
     } catch (error) {
       next(error);
     }
   };
-}
-
-// ESM import cache busting
-/**
- * @type {() => Promise<ServerBuild>}
- */
-async function reimportServer() {
-  const stat = fs.statSync(BUILD_PATH);
-
-  // use a timestamp query parameter to bust the import cache
-  return import(BUILD_PATH + "?t=" + stat.mtimeMs);
 }
